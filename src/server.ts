4485fb32b72724abe6902d391b36ba2e49c1f142
JaY4165 <harsha.gontu@gmail.com>
2024-04-04 22:10:19 +0530

    initial commit ✨

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import cors from 'cors';
+
+
+const app = express();
+const port = 8080;
+
+
+app.use(bodyParser.json({
+    limit: "1mb"
+}
+));
+app.use(express.json({
+    limit: "1mb"
+}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
